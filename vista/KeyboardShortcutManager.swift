3661ac71c0ef732d676f49c0c1ac26c4e48c0de1
Evan Chiao <evanc314@gmail.com>
2025-02-20 14:36:07 -0600

    fix keyboard shorcut toggle closes #12

--- vista/KeyboardShortcutManager.swift
+++ vista/KeyboardShortcutManager.swift
@@ -7,15 +7,36 @@ extension KeyboardShortcuts.Name {
 
 class KeyboardShortcutManager: ObservableObject {
     private let screenshotManager: ScreenshotManager
+    @AppStorage("shortcutEnabled") private var shortcutEnabled = true
     
     init(screenshotManager: ScreenshotManager) {
         self.screenshotManager = screenshotManager
         setupShortcut()
+        
+        // Observe changes to shortcutEnabled setting
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(shortcutSettingChanged),
+            name: UserDefaults.didChangeNotification,
+            object: nil
+        )
     }
     
     private func setupShortcut() {
-        KeyboardShortcuts.onKeyUp(for: .takeScreenshot) { [weak self] in
-            self?.screenshotManager.initiateScreenshot()
+        if shortcutEnabled {
+            KeyboardShortcuts.onKeyUp(for: .takeScreenshot) { [weak self] in
+                self?.screenshotManager.initiateScreenshot()
+            }
+        } else {
+            KeyboardShortcuts.disable(.takeScreenshot)
         }
+    }
+    
+    @objc private func shortcutSettingChanged() {
+        setupShortcut()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
