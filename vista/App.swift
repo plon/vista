d429b58c0433f5301d39f5bfb321c61da426feee
Evan Chiao <chiaoevan@gmail.com>
2025-03-12 16:01:12 -0500

    Update app and menu bar icons

--- vista/App.swift
+++ vista/App.swift
@@ -19,7 +19,7 @@ struct VistaApp: App {
     }
 
     var body: some Scene {
-        MenuBarExtra("vista", systemImage: "mountain.2.fill") {
+        MenuBarExtra("vista", image: "MenuBarIcon") {
             MenuBarView(screenshotManager: screenshotManager, keyboardManager: keyboardManager)
         }
     }
